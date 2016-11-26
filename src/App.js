b54f9435d4e49bc4ec850a8f28e319962c53f09e
Tim Head <betatim@gmail.com>
2016-11-26 15:22:44 +0100

    Jupp

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import DropZone from 'react-dropzone';
+import 'keras-js';
+import './App.css';
+
+
+class ImageItem extends Component {
+  render() {
+    const model = new KerasJS.Model({
+      filepaths: {
+        model: 'url/path/to/model.json',
+        weights: 'url/path/to/model_weights.buf',
+        metadata: 'url/path/to/model_metadata.json'
+      },
+    gpu: false
+    })
+    const file = this.props.file
+    return (
+      <li key={file.name}>
+        <img
+          src={file.preview}
+          alt={file.name}
+          width="60%"
+        />
+      </li>
+  )}
+}
+
+class ImageList extends Component {
+  render() {
+    return (
+      <ul>
+        {this.props.images.map(
+          (file) => <ImageItem file={file} />
+          )
+        }
+      </ul>
+    )
+  }
+}
+
+class ImageClassifier extends Component {
+  constructor() {
+    super();
+    this.state = {
+      files: [],
+    }
+    // Otherwise handleDrop can't modify the state
+    this.handleDrop = this.handleDrop.bind(this)
+  }
+  handleDrop(files) {
+    this.setState({files: files});
+  }
+
+  handleOpenClick() {
+    this.refs.dropzone.open()
+  }
+
+  render() {
+    return (
+      <div>
+        <DropZone onDrop={this.handleDrop}>
+          <div>Try dropping some files here, or click to select files to upload.</div>
+        </DropZone>
+        {this.state.files.length > 0 ?
+          <div>
+            <ImageList images={this.state.files} />
+          </div>
+        : null}
+      </div>
+    )
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Fake Image Classifier</h2>
+        </div>
+        <div>
+          <ImageClassifier />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
